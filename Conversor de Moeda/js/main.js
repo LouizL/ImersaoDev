9a3116f95949e4f6cba4ed9188e066beb5a2118a
LouizL <luizh_bl@hotmail.com>
2022-09-10 16:56:50 -0300

    Conversor de moeda - ImersãoDEV

---
+++ Conversor de Moeda/js/main.js
@@ -0,0 +1,34 @@
+function ConverterDolar() {
+    var valorElemento = document.getElementById("valorD");
+    var valorD = parseFloat(valorElemento.value);
+    console.log(typeof valorD);
+    var valorEmReal = (valorD * 5.24).toFixed(2);
+  
+    Printar(valorEmReal);
+  }
+  
+  function ConverterEuro() {
+    var valorElemento = document.getElementById("valorE");
+    var valorE = parseFloat(valorElemento.value);
+    console.log(typeof valorE);
+    var valorEmReal = (valorE * 5.25).toFixed(2);
+  
+    Printar(valorEmReal);
+  }
+  
+  function ConverterYuan() {
+    var valorElemento = document.getElementById("valorY");
+    var valorY = parseFloat(valorElemento.value);
+    console.log(typeof valorY);
+    var valorEmReal = (valorY * 0.75).toFixed(2);
+  
+    Printar(valorEmReal);
+  }
+  
+  function Printar(valorEmReal) {
+    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorConvertido = "O resultado em Real é R$ " + valorEmReal;
+    elementoValorConvertido.innerHTML = valorConvertido;
+    alert(valorConvertido);
+  }
+  
